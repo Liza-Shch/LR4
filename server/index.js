a80ff55d80a3236ef93ffe2342829e08f00e70d6
Elizaveta Shcherbakova <scherbakova.liz@yandex.ru>
2019-11-26 22:11:56 +0300

    develop: done v1

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const http = require('http');
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
+const Controllers = require('../controllers');
+const Middlewares = require('../middlewares');
+
 app.get('/api', (_, res) => res.status(200).send({ message: 'Welcome to API!' }));
+app.post('/api/post', Middlewares.Post.post, Controllers.Post.createPost);
+app.get('/api/post/:idOrIdempotencyKey', Middlewares.Post.getPost, Controllers.Post.getPost);
+app.put('/api/post', Middlewares.Post.post, Controllers.Post.updatePost);
+app.put('/api/post/delete', Middlewares.Post.post, Controllers.Post.deletePost);
+
 const server = http.createServer(app);
 server.listen(port);
